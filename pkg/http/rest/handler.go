18a3522b77d42bc59c9dfa91d0b769a3ece485ea
ehliao <en-hua.liao@ubisoft.com>
2018-10-15 05:37:27 -0400

    implemet allbuckets and createbucket api

--- pkg/http/rest/handler.go
+++ pkg/http/rest/handler.go
@@ -2,19 +2,39 @@ package rest
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
-	"gopkg.in/mgo.v2/bson"
+	//	"net/http"
 
-	"ResourcePool/configs"
-	"ResourcePool/pkg/dao"
-	"ResourcePool/pkg/models"
+	//	"gopkg.in/mgo.v2/bson"
+
+	//	"ResourcePool/configs"
+	//	"ResourcePool/pkg/dao"
+	//	"ResourcePool/pkg/models"
 
 	"github.com/gorilla/mux"
+	minio "github.com/minio/minio-go"
+	//minio "github.com/minio/minio-go"
 )
 
-var cfg = config.Config{}
-var da = dao.ImagesDAO{}
+var minioClient minio
+var location = "us-east-1"
+
+func Init() {
+	endpoint := "play.minio.io:9000"
+	accessKeyID := "Q3AM3UQ867SPQQA43P2F"
+	secretAccessKey := "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG"
+	useSSL := true
+
+	minioClient, err := minio.New(endpoint, accessKeyID, secretAccessKey, useSSL)
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+
+	log.Printf("%#v\n", minioClient)
+}
 
 func respondWithError(w http.ResponseWriter, code int, message string) {
 	respondWithJson(w, code, map[string]string{"error": message})
@@ -27,87 +47,137 @@ func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
 	w.Write(response)
 }
 
-func allImagesEndPoint(w http.ResponseWriter, r *http.Request) {
-	images, err := da.FindAll()
+func Handler() http.Handler {
+	r := mux.NewRouter()
+
+	// bucket api
+	r.HandleFunc("/buckets", allBuckets).Methods("GET")             // get bucket list
+	r.HandleFunc("/buckets/{name}", createBucket).Methods("POST")   // create bucket
+	r.HandleFunc("/buckets/{name}", updateBucket).Methods("PUT")    // update bucket
+	r.HandleFunc("/buckets/{name}", deleteBucket).Methods("DELETE") // delete bucket
+	r.HandleFunc("/buckets/{name}", listBucket).Methods("GET")      // list bucket items
+
+	// object api
+	//r.HandleFunc("/object/{bucket}/{object}", allImagesEndPoint).Methods("GET")
+	r.HandleFunc("/object/{bucket}/{object}", createObject).Methods("POST")
+	r.HandleFunc("/object/{bucket}/{object}", updateObject).Methods("PUT")
+	r.HandleFunc("/object/{bucket}/{object}", deleteObject).Methods("DELETE")
+	r.HandleFunc("/object/{bucket}/{object}", findObject).Methods("GET")
+
+	return r
+}
+
+func allBuckets(w http.ResponseWriter, r *http.Request) {
+	buckets, err := minioClient.ListBucket()
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 
-	respondWithJson(w, http.StatusOK, images)
+	respondWithJson(w, http.StatusOK, buckets)
 }
 
-func findImagesEndPoint(w http.ResponseWriter, r *http.Request) {
+func createBucket(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
-	image, err := da.FindById(params["id"])
-	if err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid image ID")
-		return
-	}
-	respondWithJson(w, http.StatusOK, image)
-}
-
-func createImagesEndPoint(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	var image models.Image
-	if err := json.NewDecoder(r.Body).Decode(&image); err != nil {
+	bucketName := params["bucket"]
+	if err := minioClient.MakeBucket(bucketName, location); err != nil {
 		respondWithError(w, http.StatusBadRequest, err.Error())
-		return
-	}
-
-	image.ID = bson.NewObjectId()
-
-	if err := da.Insert(image); err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 
 	respondWithJson(w, http.StatusCreated, image)
 }
 
-func updateImagesEndPoint(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	var image models.Image
-	if err := json.NewDecoder(r.Body).Decode(&image); err != nil {
-		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
-		return
-	}
-	if err := da.Update(image); err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
+func updateBucket(w http.ResponseWriter, r *http.Request) {
+	//	defer r.Body.Close()
+	//	var image models.Image
+	//	if err := json.NewDecoder(r.Body).Decode(&image); err != nil {
+	//		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+	//		return
+	//	}
+	//	if err := da.Update(image); err != nil {
+	//		respondWithError(w, http.StatusInternalServerError, err.Error())
+	//		return
+	//	}
 	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
 }
 
-func deleteImagesEndPoint(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	var image models.Image
-	if err := json.NewDecoder(r.Body).Decode(&image); err != nil {
-		respondWithError(w, http.StatusBadRequest, err.Error())
-		return
-	}
-	if err := da.Delete(image); err != nil {
-		respondWithError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
+func deleteBucket(w http.ResponseWriter, r *http.Request) {
+	//	defer r.Body.Close()
+	//	var image models.Image
+	//	if err := json.NewDecoder(r.Body).Decode(&image); err != nil {
+	//		respondWithError(w, http.StatusBadRequest, err.Error())
+	//		return
+	//	}
+	//	if err := da.Delete(image); err != nil {
+	//		respondWithError(w, http.StatusInternalServerError, err.Error())
+	//		return
+	//	}
 	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
 }
 
-func Init() {
-	cfg.Read()
-
-	da.Server = cfg.Server
-	da.Database = cfg.Database
-	da.Connect()
+func listBucket(w http.ResponseWriter, r *http.Request) {
+	//	params := mux.Vars(r)
+	//	image, err := da.FindById(params["id"])
+	//	if err != nil {
+	//		respondWithError(w, http.StatusBadRequest, "Invalid image ID")
+	//		return
+	//	}
+	respondWithJson(w, http.StatusOK, image)
 }
 
-func Handler() http.Handler {
-	r := mux.NewRouter()
-	r.HandleFunc("/images", allImagesEndPoint).Methods("GET")
-	r.HandleFunc("/images", createImagesEndPoint).Methods("POST")
-	r.HandleFunc("/images", updateImagesEndPoint).Methods("PUT")
-	r.HandleFunc("/images", deleteImagesEndPoint).Methods("DELETE")
-	r.HandleFunc("/images/{id}", findImagesEndPoint).Methods("GET")
+func createObject(w http.ResponseWriter, r *http.Request) {
+	//	defer r.Body.Close()
+	//	var image models.Image
+	//	if err := json.NewDecoder(r.Body).Decode(&image); err != nil {
+	//		respondWithError(w, http.StatusBadRequest, err.Error())
+	//		return
+	//	}
 
-	return r
+	//	image.ID = bson.NewObjectId()
+
+	//	if err := da.Insert(image); err != nil {
+	//		respondWithError(w, http.StatusInternalServerError, err.Error())
+	//		return
+	//	}
+
+	respondWithJson(w, http.StatusCreated, image)
+}
+
+func updateObject(w http.ResponseWriter, r *http.Request) {
+	//	defer r.Body.Close()
+	//	var image models.Image
+	//	if err := json.NewDecoder(r.Body).Decode(&image); err != nil {
+	//		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+	//		return
+	//	}
+	//	if err := da.Update(image); err != nil {
+	//		respondWithError(w, http.StatusInternalServerError, err.Error())
+	//		return
+	//	}
+	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
+}
+
+func deleteObject(w http.ResponseWriter, r *http.Request) {
+	//	defer r.Body.Close()
+	//	var image models.Image
+	//	if err := json.NewDecoder(r.Body).Decode(&image); err != nil {
+	//		respondWithError(w, http.StatusBadRequest, err.Error())
+	//		return
+	//	}
+	//	if err := da.Delete(image); err != nil {
+	//		respondWithError(w, http.StatusInternalServerError, err.Error())
+	//		return
+	//	}
+	respondWithJson(w, http.StatusOK, map[string]string{"result": "success"})
+}
+
+func listObject(w http.ResponseWriter, r *http.Request) {
+	//	params := mux.Vars(r)
+	//	image, err := da.FindById(params["id"])
+	//	if err != nil {
+	//		respondWithError(w, http.StatusBadRequest, "Invalid image ID")
+	///		return
+	//	}
+	respondWithJson(w, http.StatusOK, image)
 }
